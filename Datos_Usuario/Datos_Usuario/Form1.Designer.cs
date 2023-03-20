7ed477dcebfeca1064bca483b12f5024d920763a
angelGarza2004salas <125837002+angelGarza2004salas@users.noreply.github.com>
2023-03-20 14:27:44 -0600

    Add files via upload

---
+++ Datos_Usuario/Datos_Usuario/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Datos_Usuario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPaterno = new System.Windows.Forms.TextBox();
+            this.txtMaterno = new System.Windows.Forms.TextBox();
+            this.nudEdad = new System.Windows.Forms.NumericUpDown();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEdad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(180, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(180, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido paterno";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(180, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido materno";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(180, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 27);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Edad";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(341, 68);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(279, 30);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtPaterno
+            // 
+            this.txtPaterno.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPaterno.Location = new System.Drawing.Point(341, 128);
+            this.txtPaterno.Name = "txtPaterno";
+            this.txtPaterno.Size = new System.Drawing.Size(279, 30);
+            this.txtPaterno.TabIndex = 5;
+            // 
+            // txtMaterno
+            // 
+            this.txtMaterno.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaterno.Location = new System.Drawing.Point(341, 181);
+            this.txtMaterno.Name = "txtMaterno";
+            this.txtMaterno.Size = new System.Drawing.Size(279, 30);
+            this.txtMaterno.TabIndex = 6;
+            // 
+            // nudEdad
+            // 
+            this.nudEdad.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudEdad.Location = new System.Drawing.Point(341, 244);
+            this.nudEdad.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudEdad.Name = "nudEdad";
+            this.nudEdad.ReadOnly = true;
+            this.nudEdad.Size = new System.Drawing.Size(279, 30);
+            this.nudEdad.TabIndex = 7;
+            this.nudEdad.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(454, 299);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(166, 56);
+            this.btnEnviar.TabIndex = 8;
+            this.btnEnviar.Text = "Enviar archivo";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(454, 361);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(166, 56);
+            this.btnMostrar.TabIndex = 9;
+            this.btnMostrar.Text = "Mostrar archivo";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.nudEdad);
+            this.Controls.Add(this.txtMaterno);
+            this.Controls.Add(this.txtPaterno);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudEdad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPaterno;
+        private System.Windows.Forms.TextBox txtMaterno;
+        private System.Windows.Forms.NumericUpDown nudEdad;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ Datos_Usuario/Datos_Usuario/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datos_Usuario
+{
+    public partial class Form1 : Form
+    {
+        // estoy utilizando clases que me permiten escribir un archivo binario y leerlo (binaryreader y binarywriter)
+        // ademas de utilizar filestream para manejar los archivos que se han creado o que se vayan a utilizar en la aplicacion
+        // y un openfiledialog que me ayuda a manejar la ruta de un archivo existente
+        private BinaryReader lectorDeBinarios;
+        private BinaryWriter escritorDeBinarios;
+        private FileStream manejoDeArchivos;
+        private OpenFileDialog rutaAbierta;
+        private SaveFileDialog rutaGuardada;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            // como vimos en clase detecto el error en lugar de esperar a que suceda ese error
+            // aqui si no se llena alguno de los datos del formulario no permito que se cree un archivo
+            // porque no tendria datos que meter en ese archivo
+            if (txtMaterno == null || txtNombre == null || txtPaterno == null)
+            {
+                MessageBox.Show("Error al escoger la ruta de archivo. ", "Error", MessageBoxButtons.OK);
+                return;
+            }     
+
+            // ruta seleccionada por el usuario en la que se guardara el archivo
+            rutaGuardada = new SaveFileDialog();
+            rutaGuardada.Filter = "Archivos binarios (*.bin)|*.bin";
+            rutaGuardada.Title = "Datos del usuario";
+
+            // como vimos en clase detecto el error en lugar de esperar a que suceda ese error
+            // aqui si no se escogiera una ruta no se crearia el archivo porque no le indicariamos que escribir en el
+            if (rutaGuardada.ShowDialog() == DialogResult.Cancel)
+            {
+                MessageBox.Show("Error al escoger la ruta de archivo. ", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            // relleno los parametros de la clase manejadora de archivos (filestream)
+            // en el parametro 1 le entrego la ruta que escogi con el showdialog, en el 2 indico que quiero crear el archivo 
+            // y en el tercero indico que quiero escribir en el archivo
+            manejoDeArchivos = new FileStream(rutaGuardada.FileName, FileMode.Create, FileAccess.Write);
+            escritorDeBinarios = new BinaryWriter(manejoDeArchivos);
+
+            // con el escritor de archivos (binarywriter) escribo el archivo
+            escritorDeBinarios.Write(txtNombre.Text + " " + txtPaterno.Text + " " + txtMaterno.Text + " " + nudEdad.Value.ToString());
+            
+            // cierro o termino la escritura y el manejo de los archivos despues de leerlos
+            escritorDeBinarios.Close();
+            manejoDeArchivos.Close();
+
+            // despues de escribir los datos y crear el archivo muestro una ventana emergente con los datos del usuario
+            // ingresados en esa sesion
+            MessageBox.Show("Tus datos son: " + txtNombre.Text + " " + txtPaterno.Text + " " + txtMaterno.Text + " " + nudEdad.Text, "Datos del usuario", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Tu archivo con estos datos se guardo correctamente en la ruta seleccionada", "Guardado", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            // filtrando los archivos (txt, dll, etc) para ver unicamente los archivos binarios que hay en la computadora
+            rutaAbierta = new OpenFileDialog();
+            rutaAbierta.FileName = "Datos de usuario";
+            rutaAbierta.Filter = "Archivos de binarios (*.bin)|*.bin";
+
+            if (rutaAbierta.ShowDialog() == DialogResult.OK)
+            {
+                // relleno los parametros de la clase manejadora de archivos (filestream)
+                // en el parametro 1 le entrego la ruta que escogi con el showdialog, en el 2 indico que quiero abrir el archivo 
+                // y en el tercero indico que quiero leer el archivo
+                manejoDeArchivos = new FileStream(rutaAbierta.FileName, FileMode.Open, FileAccess.Read);
+                lectorDeBinarios = new BinaryReader(manejoDeArchivos);
+
+                // guardo la linea de datos leida´por el lector de archivos al que le indico leer datos de tipo string
+                string datosDeUsuario = lectorDeBinarios.ReadString();
+
+                // usando la variable datosDeUsuario muestro los datos leidos en el archivo seleccionado
+                MessageBox.Show(datosDeUsuario, "Datos de un usuario anterior", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Puede seguir creando mas archivos", "Puede continuar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lectorDeBinarios.Close();
+                manejoDeArchivos.Close();
+            }
+            else
+            {
+                MessageBox.Show("No se pudo leer el archivo", "Ocurrio un problema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // cierro o termino la lectura y el manejo de los archivos despues de leerlos
+          
+        }
+    }
+}
